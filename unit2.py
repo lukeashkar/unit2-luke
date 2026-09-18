03360b5d7face1dd6c4815f86961436f81ca6769
lukeashkar <lukea45@nycstudents.net>
2026-09-18 10:32:37 -0400

    Update unit2.py

--- unit2.py
+++ unit2.py
@@ -1,3 +1,18 @@
-bill=float(input( "how much the bill bro."))
+""" bill=float(input( "how much the bill bro."))
 tip=int(input("how much you tipping"))
 print(bill+tip)
+print(f"your total is {bill+tip} you better pay up") """
+
+
+#make a code using the input to make them say a sentence
+#use the len and .split to make it so they count words
+""" 
+sentence= input("enter a sentence")
+word_count=len(sentence.split())
+print(word_count) """
+
+
+odd_or_even=input("pick a number")
+odd_or_even=int
+if odd_or_even%2==0:
+    print("even")
