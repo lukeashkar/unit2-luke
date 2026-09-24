ed9a78dacce6cb34abfc06dc9d572e58b8475530
lukeashkar <lukea45@nycstudents.net>
2026-09-24 10:13:41 -0400

    Update notes.py

---
+++ notes.py
@@ -0,0 +1,20 @@
+""" rain=False
+if rain==True:
+    print("bring umbrella)")
+else:
+    print("no umbrella")
+
+age=66
+if age > 65:
+    print("senior citizen")
+elif age > 18:
+    print ("adult")
+else:
+    print("child")
+
+has_key=False
+is_magic=True
+if has_key and is_magic:
+    print("enter castle")
+else:
+    print("cannot enter") """
